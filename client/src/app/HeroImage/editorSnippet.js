ea9e5ac8af6544e63ebb6c242ebb82f976a17349
Victoria Benoit <vrose834@gmail.com>
2026-04-06 21:33:22 -0300

    Started on the home page

---
+++ client/src/app/HeroImage/editorSnippet.js
@@ -0,0 +1,4 @@
+// This is a simple code snippet to animate in the text editor window
+export function helloWorld() {
+  console.log("Hello, world!");
+}
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,12 +1,11 @@
+import HeroImage from "./HeroImage/HeroImage.component";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <div className={styles.intro}>
-          <h1>Coming Soon...</h1>
-        </div>
+      <main>
+        <HeroImage />
       </main>
     </div>
   );
