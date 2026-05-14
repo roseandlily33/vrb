8fbabe7b1bdc83e476bd84b567f9544d4712b0ea
Victoria Benoit <vrose834@gmail.com>
2026-05-14 15:58:07 -0300

    Added in the contact form again

--- client/src/app/contact/page.jsx
+++ client/src/app/contact/page.jsx
@@ -1,4 +1,4 @@
-// import ContactForm from "./ContactForm/ContactForm.component";
+import ContactForm from "./ContactForm/ContactForm.component";
 import ContactInfo from "./ContactInfo/ContactInfo.component";
 import Faq from "./FAQ/Faq.component";
 import styles from "./page.module.css";
@@ -7,7 +7,7 @@ export default function Contact() {
   return (
     <main className={styles.contactPage}>
       <div className={styles.sideBySide}>
-        {/* <ContactForm /> */}
+        <ContactForm />
         <ContactInfo />
       </div>
       <Faq />
