210f0871e6bf52d49f41319e77f22eb081f37731
Victoria Benoit <vrose834@gmail.com>
2026-05-30 09:50:41 -0300

    issue with optional add ons

--- client/src/app/package/[slug]/page.jsx
+++ client/src/app/package/[slug]/page.jsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 import { PackageInfo } from "../Packages/packageList";
 import designPackages from "../DesignPackage/designPackage";
 import CTA3 from "../../Components/CTA/CTA3/CTA3.component";
-import OptionalAddOns from "./OptionalAddOns/optionalAddOns.component";
+import OptionalAddOns from "./OptionalAddOns/OptionalAddOns.component";
 import Breadcrumbs from "../../case-study/[project]/Components/Extras/Breadcrumbs/Breadcrumbs.component";
 
 export default async function PackagePage({ params, searchParams }) {
